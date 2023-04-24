03574de028ad998501f0c3fd8b0ea3f4eb277094
Pedro-Ivo-Neves <83455817+Pedro-Ivo-Neves@users.noreply.github.com>
2023-04-24 02:34:34 +0200

    LexicalAnalysis improved
    Melhorou-se a class para poder classificar com mais precisão com as conexões

--- Compiler/src/analysis/LexicalAnalysis.java
+++ Compiler/src/analysis/LexicalAnalysis.java
@@ -2,21 +2,17 @@ package analysis;
 import java.util.ArrayList;
 import java.util.regex.Pattern;
 
-import analysis.exception.LexicalExcepction;
+import analysis.exception.LexicalException;
 import constants.*;
+import models.*;
 import views.*;
 
 public class LexicalAnalysis {
     
-    private ArrayList<String> tokenList;
-    private ArrayList<String> classificationList;
-    private KeyWords keyWords;
+    private ArrayList<Token_Model> tokenList;
 
-
-    public LexicalAnalysis(ArrayList<String> listaPalavras){
+    public LexicalAnalysis(ArrayList<Token_Model> listaPalavras){
         this.tokenList = listaPalavras;
-        this.classificationList = new ArrayList<String>();
-        this.keyWords = new KeyWords();
         lexicalMap();
     }
 
@@ -24,26 +20,24 @@ public class LexicalAnalysis {
 
         tokenList.forEach((token)->{
 
-            
-            if (Pattern.compile("[^a-zA-Z0-9]").matcher(token).find()) {
-                
-                if(keyWords.simbolosEspeciais.contains(token)){
-                    classificationList.add("SE");
-                } else{
-                    throw new LexicalExcepction("O simbolo \033[31m"+token+"\033[0m nao pertence");
-                }
+            int index =tokenList.indexOf(token);
+
+            if (Pattern.compile("[^a-zA-Z0-9]").matcher(token.getToken()).find()) {
+
+                if(KeyWords.simbolosEspeciais.contains(token.getToken())){
+                    tokenList.set(index, new SE_Model(token).setBeforeToken(index==0 ? null : tokenList.get(index-1)));
+                } 
 
             } else {
-                if(keyWords.tiposPrimitivos.contains(token)){
-                    classificationList.add("TP");
+                if(KeyWords.tiposPrimitivos.contains(token.getToken())){
+                    tokenList.set(index, new TP_Model(token).setBeforeToken(index==0 ? null : tokenList.get(index-1)));
                 }
 
                 else{
-                    if (keyWords.palavrasReservadas.contains(token)) {
-                        classificationList.add("PR");
+                    if (KeyWords.palavrasReservadas.contains(token.getToken())) {
+                        tokenList.set(index, new PR_Model(token).setBeforeToken(index==0 ? null : tokenList.get(index-1)));
                     } else {
-                        classificationList.add("VR");
-                    //    throw new LexicalExcepction("A palavra \033[31m"+token+"\033[0m nao pertence");
+                        tokenList.set(index, new VR_Model(token).setBeforeToken(index==0 ? null : tokenList.get(index-1)));
                     }
                 }
             }
@@ -51,6 +45,6 @@ public class LexicalAnalysis {
     }
 
     public void lexicalTable(){
-        new TablesView().TokenTableView(this.tokenList, this.classificationList);
+        new TablesView().TokenTableView(this.tokenList);
     }
 }
