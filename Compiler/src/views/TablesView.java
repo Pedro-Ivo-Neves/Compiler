16bbee76308aadb7daa47f6f0eba4ba7930cc094
Pedro-Ivo-Neves <83455817+Pedro-Ivo-Neves@users.noreply.github.com>
2023-04-24 02:33:12 +0200

    TokenTableView imporved
    Modificou-se para poder receber a lista de Tokens e mostrar as Tokens e as suas classificações específicas

--- Compiler/src/views/TablesView.java
+++ Compiler/src/views/TablesView.java
@@ -2,19 +2,22 @@ package views;
 
 import java.util.ArrayList;
 
+import models.Token_Model;
+
 public class TablesView {
     
     public TablesView(){}
 
-    public void TokenTableView(ArrayList<String> token, ArrayList<String> classification){
+    public void TokenTableView(ArrayList<Token_Model> tokens){
         System.out.println("====================================");
         System.out.println("|    \033[36mTOKEN\033[0m    |    \033[35mCLASSIFICACAO\033[0m   |");
         System.out.println("====================================");
         
-        for (int i = 0; i < token.size(); i++) {
-            System.out.printf("| %10s  |    %8s        |", token.get(i), classification.get(i));
+        tokens.forEach((token)->{
+            System.out.printf("| %10s  |    %8s        |", token.getToken(), token.getType());
             System.out.println();
-        }
+        });
+
         System.out.println("====================================");
     }
 }
