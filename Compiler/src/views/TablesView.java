5f954f9555b9a0ac7b003d725023ba55c6eab3a9
Pedro-Ivo-Neves <83455817+Pedro-Ivo-Neves@users.noreply.github.com>
2023-04-28 20:28:34 +0200

    Better TableView

--- Compiler/src/views/TablesView.java
+++ Compiler/src/views/TablesView.java
@@ -9,15 +9,15 @@ public class TablesView {
     public TablesView(){}
 
     public void TokenTableView(ArrayList<Token_Model> tokens){
-        System.out.println("====================================");
-        System.out.println("|    \033[36mTOKEN\033[0m    |    \033[35mCLASSIFICACAO\033[0m   |");
-        System.out.println("====================================");
+        System.out.println("=======================================================================");
+        System.out.println("|    \033[36mTOKEN\033[0m    |    \033[35mCLASSIFICACAO\033[0m   |    \033[32mNr Linha\033[0m    |    \033[31mNr Coluna\033[0m    |");
+        System.out.println("=======================================================================");
         
         tokens.forEach((token)->{
-            System.out.printf("| %10s  |    %8s        |", token.getToken(), token.getType());
+            System.out.printf("| %10s  |    %8s        |    %5d       |     %5d       |", token.getToken(), token.getType(),token.getLineIndex(), token.getColumnIndex());
             System.out.println();
         });
 
-        System.out.println("====================================");
+        System.out.println("=======================================================================");
     }
 }
