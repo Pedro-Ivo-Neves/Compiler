908b2872571e41da1612df08111bd47a94fbc974
Pedro-Ivo-Neves <83455817+Pedro-Ivo-Neves@users.noreply.github.com>
2023-05-01 02:06:04 +0200

    Correct Execute

--- Compiler/src/Executar.java
+++ Compiler/src/Executar.java
@@ -1,53 +1,13 @@
-import java.util.ArrayList;
+// import java.util.ArrayList;
 
 import analysis.LexicalAnalysis;
 import constants.KeyWords;
+
 import functions.*;
 
 public class Executar {
     public static void main(String[] args) {
-        // new KeyWords();
-        // new LexicalAnalysis(new Ficheiro(".\\docs\\programs\\main3.txt").readCode()).lexicalTable();
-        // int i =0;
-        // while (i<10) {
-        //     if (i<3) {
-        //         System.out.println("I: "+i);
-        //         i++;
-        //     } else {
-        //         break;
-        //     }
-        // }
-
-        var sb = new StringBuilder();
-
-        var list = new ArrayList<String>();
-
-        int columnIndex=1;
-
-        String p = "Lol Lki";
-
-        
-        System.out.println("Length: "+p.length());
-        for (char letra : p.toCharArray()) {
-            columnIndex++;
-            if (letra==' ' || columnIndex==p.length()+1) {
-                if(columnIndex==p.length()+1){
-                    sb.append(letra);
-                }
-                String palavra = (sb.length()!=0) ? sb.toString():null;
-
-                if(palavra.equals(null)){
-                    continue;
-                } else{
-                    list.add(palavra);
-                    sb.delete(0, sb.length());
-                }
-
-            } else{
-                sb.append(letra);
-            }
-        }
-
-        list.forEach((l)->System.out.println("As Palavras sao: "+l));
+        new KeyWords();
+        new LexicalAnalysis(new FilesFunc(".\\docs\\programs\\main4.java").readCode()).lexicalTable();
     }
 }
