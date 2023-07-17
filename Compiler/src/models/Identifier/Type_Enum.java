75f84d9b78d6c6c685a9ae1fddd6e5510d58c213
Pedro-Ivo-Neves <83455817+Pedro-Ivo-Neves@users.noreply.github.com>
2023-07-17 17:48:09 +0200

    Type_Enum

---
+++ Compiler/src/models/Identifier/Type_Enum.java
@@ -0,0 +1,16 @@
+package models.Identifier;
+
+public enum Type_Enum {
+    
+    STR("String"),
+    CHAR("char"),
+    INT("int"),
+    DOUBLE("double"),
+    FLOAT("float"),
+    LONG("long"),
+    BOOL("boolean"),
+    VOID("void")
+    ;
+
+    Type_Enum(String type){}
+}
