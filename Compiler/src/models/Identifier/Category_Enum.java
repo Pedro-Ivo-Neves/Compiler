8c4691ff9deaf26916cc766b82ec94dacf659039
Pedro-Ivo-Neves <83455817+Pedro-Ivo-Neves@users.noreply.github.com>
2023-07-17 17:38:19 +0200

    Category_Enum

---
+++ Compiler/src/models/Identifier/Category_Enum.java
@@ -0,0 +1,11 @@
+package models.Identifier;
+
+public enum Category_Enum {
+    
+    CLASS("class"),
+    FUNC("function"),
+    PARAM("parameter"),
+    VAR("variable");
+
+    Category_Enum(String category){}
+}
